8cac8b49326ec1af6eb61c1f2f733897ac9ef26d
Anastasiya <36969439+radvilovich@users.noreply.github.com>
2019-10-27 17:44:31 +0300

    feat: add picture2 (32x32)

--- script.js
+++ script.js
@@ -2,27 +2,41 @@ const canvas = document.getElementById("drawingCanvas");
 const ctx = canvas.getContext("2d");
 const width = canvas.width;
 const height = canvas.height;
+url1 = 'https://raw.githubusercontent.com/rolling-scopes-school/tasks/master/tasks/stage-2/codejam-canvas/data/4x4.json';
+url2 = 'https://raw.githubusercontent.com/rolling-scopes-school/tasks/master/tasks/stage-2/codejam-canvas/data/32x32.json';
 
-//picture1(ctx, width, height);
+async function getPicture(url){
+    const response = await fetch(url);
+    return await response.json();
+}
 
-function picture1(ctx, width, height) {
-    const rectWidth = width / 4;
-    const rectHeight = height / 4;
-    const colors = [
-        ["00BCD4", "FFEB3B","FFEB3B","00BCD4"],
-        ["FFEB3B", "FFC107","FFC107","FFEB3B"],
-        ["FFEB3B", "FFC107","FFC107","FFEB3B"],
-        ["00BCD4", "FFEB3B","FFEB3B","00BCD4"]
-    ];
+getPicture(url1);
+
+//picture(ctx, width, height, 4, url1);
+//picture(ctx, width, height, 32, url2);
+
+function getColor(color) {
+    if(typeof color === 'string') {
+        return '#' + color;
+    } else {
+        return `rgb(${color[0]}, ${color[1]}, ${color[2]})`;
+    }
+}
+
+async function picture(ctx, width, height, pictureCount, url) {
+    const rectWidth = width / pictureCount;
+    const rectHeight = height / pictureCount;
+    const colors = await getPicture(url);
     let startX = 0;
     let startY = 0;
     colors.forEach((line) => {
         startX = 0;
         line.forEach((color) => {
+            const colorString = getColor(color);
             ctx.beginPath();
             ctx.rect(startX, startY, rectWidth, rectHeight);
             startX += rectWidth;
-            ctx.fillStyle = "#" + color;
+            ctx.fillStyle = colorString;
             ctx.fill();
             ctx.closePath();
         });
