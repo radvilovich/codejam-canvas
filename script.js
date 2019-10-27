16bb2c88778817f25ce4ee701f9f927fe471dc57
Anastasiya <36969439+radvilovich@users.noreply.github.com>
2019-10-27 18:24:40 +0300

    feat: add Picture3 (The Rolling Scopes)

--- script.js
+++ script.js
@@ -4,13 +4,12 @@ const width = canvas.width;
 const height = canvas.height;
 url1 = 'https://raw.githubusercontent.com/rolling-scopes-school/tasks/master/tasks/stage-2/codejam-canvas/data/4x4.json';
 url2 = 'https://raw.githubusercontent.com/rolling-scopes-school/tasks/master/tasks/stage-2/codejam-canvas/data/32x32.json';
+url3 = 'https://raw.githubusercontent.com/rolling-scopes-school/tasks/master/tasks/stage-2/codejam-canvas/data/image.png';
 
 async function getPicture(url){
     const response = await fetch(url);
     return await response.json();
 }
-
-getPicture(url1);
 
 //picture(ctx, width, height, 4, url1);
 //picture(ctx, width, height, 32, url2);
@@ -42,4 +41,19 @@ async function picture(ctx, width, height, pictureCount, url) {
         });
         startY += rectHeight;
     });
+}
+
+picture3(ctx, width, height);
+function picture3(ctx, width, height) {
+    // Создаем объект изображения
+    const img = new Image();
+
+    // Привязываем функцию к событию onload
+    // Это указывает браузеру, что делать, когда изображение загружено
+        img.onload = function() {
+	    ctx.drawImage(img, 0, 0, width, height);
+    };
+
+    // Загружаем файл изображения
+    img.src = url3;
 }
